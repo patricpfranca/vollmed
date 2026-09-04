fd8936cc3c5e804737adec6f72e82c43b03a8739
Patric Pereira <patric.franca@petanjo.com>
2026-09-04 08:39:03 -0300

    Refactor ContentView to use TabView and add MyAppointmentsView
    - Replaced the previous VStack layout in ContentView with a TabView for better navigation.
    - Introduced MyAppointmentsView to display user appointments, including a navigation title.
    - Updated VollmedApp to launch with ContentView instead of directly with HomeView.

---
+++ Vollmed/Views/MyAppointmentsView.swift
@@ -0,0 +1,22 @@
+//
+//  MyAppointmentsView.swift
+//  Vollmed
+//
+//  Created by Patric Pereira on 04/09/26.
+//
+
+import SwiftUI
+
+struct MyAppointmentsView: View {
+    var body: some View {
+        VStack {
+            Text("Minhas consultas")
+        }
+        .navigationTitle("Minhas consultas")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+#Preview {
+    MyAppointmentsView()
+}
