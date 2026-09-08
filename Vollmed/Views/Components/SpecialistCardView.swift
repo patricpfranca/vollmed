7bc1d13ae4ce5139dc7597a4636628f9c65a85ac
Patric Pereira <patric.franca@petanjo.com>
2026-09-08 16:56:12 -0300

    Add CancelAppointmentView for appointment cancellation functionality
    - Introduced CancelAppointmentView to allow users to specify a reason for canceling their appointments.
    - Integrated CancelAppointmentView into SpecialistCardView, enabling navigation to the cancellation screen.
    - Updated project structure to include the new view file.

--- Vollmed/Views/Components/SpecialistCardView.swift
+++ Vollmed/Views/Components/SpecialistCardView.swift
@@ -57,11 +57,11 @@ struct SpecialistCardView: View {
                         ButtonView(text: "Remarcar")
                     }
                     
-                    Button(action: {
-                        print("Botão de cancelar pressionado")
-                    }, label: {
+                    NavigationLink {
+                        CancelAppointmentView()
+                    } label: {
                         ButtonView(text: "Cancelar", buttonType: .cancel)
-                    })
+                    }
                 }
             } else {
                 NavigationLink {
