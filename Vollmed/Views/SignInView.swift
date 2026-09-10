53b07a5b49947b6dc61dc7b42888c963beb9334d
Patric Pereira <patric.franca@petanjo.com>
2026-09-10 09:41:10 -0300

    Implement patient login functionality in WebService and SignInView
    - Added loginPatient method in WebService to handle patient login via a POST request.
    - Updated SignInView to include a login method that utilizes the new loginPatient function.
    - Implemented alert feedback for login errors to enhance user experience.

--- Vollmed/Views/SignInView.swift
+++ Vollmed/Views/SignInView.swift
@@ -11,6 +11,22 @@ struct SignInView: View {
     
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var showAlert: Bool = false
+    
+    let service = WebService()
+    
+    func login() async {
+        do {
+            if let response = try await service.loginPatient(email: email, password: password) {
+                print(response)
+            } else {
+                showAlert = true
+            }
+        } catch {
+            showAlert = true
+            print("Ocorreu um erro no login \(error)")
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16.0) {
@@ -36,7 +52,9 @@ struct SignInView: View {
             TextFieldLabelView(label: "Senha", placeholder: "Insira sua senha", value: $password, isSecure: true)
             
             Button(action: {
-                //
+                Task {
+                    await login()
+                }
             }, label: {
                 ButtonView(text: "Entrar")
             })
@@ -52,6 +70,14 @@ struct SignInView: View {
         }
         .padding()
         .navigationBarBackButtonHidden()
+        .alert("Ops, algo deu errado!", isPresented: $showAlert) {
+            Button(action: {}, label: {
+                Text("OK")
+            })
+        } message: {
+            Text("Houve um erro ao entrar na sua conta. Por favor, tente novamente.")
+        }
+
     }
 }
 
