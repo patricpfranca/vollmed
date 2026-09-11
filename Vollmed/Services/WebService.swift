50fcecf9c8787820f5b0ace6ba9f47bd82c607a2
Patric Pereira <patric.franca@petanjo.com>
2026-09-11 08:22:20 -0300

    Implement token management in WebService and update appointment views
    - Removed hardcoded patient ID and added token retrieval from UserDefaults in WebService methods for appointment management.
    - Updated MyAppointmentsView and ScheduleAppointmentView to retrieve patient ID from UserDefaults before making API calls.
    - Ensured authorization headers are set with the retrieved token for all relevant requests.

--- Vollmed/Services/WebService.swift
+++ Vollmed/Services/WebService.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 
-let patientID = "df19e157-9717-4cf2-ab8f-935c51af6306"
-
 struct WebService {
     
     private let baseURL = "http://localhost:3001"
@@ -69,6 +67,11 @@ struct WebService {
             return false
         }
         
+        guard let token = UserDefaultsHelper.get(for: "token") else {
+            print("Token não informado")
+            return false
+        }
+        
         let requestData: [String: String] = ["motivoCancelamento": reasonToCancel]
         
         let jsonData = try JSONSerialization.data(withJSONObject: requestData)
@@ -76,6 +79,7 @@ struct WebService {
         var request = URLRequest(url: url)
         request.httpMethod = "DELETE"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.httpBody = jsonData
         
         let (_, response) = try await URLSession.shared.data(for: request)
@@ -96,6 +100,11 @@ struct WebService {
             return nil
         }
         
+        guard let token = UserDefaultsHelper.get(for: "token") else {
+            print("Token não informado")
+            return nil
+        }
+        
         let requestData: [String: String] = ["data": date]
         
         let jsonData = try JSONSerialization.data(withJSONObject: requestData)
@@ -103,6 +112,7 @@ struct WebService {
         var request = URLRequest(url: url)
         request.httpMethod = "PATCH"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.httpBody = jsonData
         
         let (data, _) = try await URLSession.shared.data(for: request)
@@ -115,12 +125,21 @@ struct WebService {
     func getAllAppointmentsFromPatient(patientID: String) async throws -> [Appointment]? {
         let endpoint = "\(baseURL)/paciente/\(patientID)/consultas"
         
+        guard let token = UserDefaultsHelper.get(for: "token") else {
+            print("Token não informado")
+            return nil
+        }
+        
         guard let url = URL(string: endpoint) else {
             print("Erro na URL!")
             return nil
         }
         
-        let (data, _) = try await URLSession.shared.data(from: url)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
         
         let appointments = try JSONDecoder().decode([Appointment].self, from: data)
         
@@ -137,6 +156,11 @@ struct WebService {
             return nil
         }
         
+        guard let token = UserDefaultsHelper.get(for: "token") else {
+            print("Token não informado")
+            return nil
+        }
+        
         let appointment = ScheduleAppointmentRequest(specialist: specialistID, patient: patientID, date: date)
         
         let jsonData = try JSONEncoder().encode(appointment)
@@ -144,6 +168,7 @@ struct WebService {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         request.httpBody = jsonData
         
         let (data, _) = try await URLSession.shared.data(for: request)
