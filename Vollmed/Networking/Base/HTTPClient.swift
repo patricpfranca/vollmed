520a55c860671279efa3d7aff5441cb3d3804476
Patric Pereira <patric.franca@petanjo.com>
2026-09-15 08:31:48 -0300

    Update Endpoint and HTTPClient protocols to support optional request body
    - Modified the Endpoint protocol to make the body property optional, allowing for requests without a body.
    - Updated the HTTPClient protocol's sendRequest method to accept an optional response model type, enhancing flexibility in handling responses.
    - Implemented the logic to conditionally set the HTTP body in the sendRequest method based on the presence of the body in the endpoint.

--- Vollmed/Networking/Base/HTTPClient.swift
+++ Vollmed/Networking/Base/HTTPClient.swift
@@ -8,5 +8,46 @@
 import Foundation
 
 protocol HTTPClient {
-    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, RequestError>
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type?) async -> Result<T, RequestError>
+}
+
+extension HTTPClient {
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type?) async -> Result<T, RequestError> {
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
+        urlComponents.port = 3000
+        
+        guard let url = urlComponents.url else {
+            return .failure(.invalidURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.allHTTPHeaderFields = endpoint.header
+        
+        if let body = endpoint.body {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+            
+            guard let response = response as? HTTPURLResponse else {
+                return .failure(.noResponse)
+            }
+            
+            switch response.statusCode {
+            case 200...299:
+                guard let responseModel = responseModel else {
+                    return .success(nil)
+                }
+            }
+        } catch {
+            
+        }
+        
+    }
 }
